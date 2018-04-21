768aa76ba2bbbd329159ff78f4636b782a4eb809
Jiri Spac <capajj@gmail.com>
2018-04-21 22:01:59 +0200

    changed dep from graphql-server-koa to apollo-server-koa

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import Koa from 'koa'
 import KoaRouter from 'koa-router'
 import koaBody from 'koa-bodyparser'
-import { graphqlKoa } from 'graphql-server-koa'
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
 import { makeExecutableSchema } from 'graphql-tools'
 import fs from 'fs'
 import koaLogger from 'koa-logger'
@@ -15,21 +15,15 @@ app.use(koaLogger())
 const router = new KoaRouter()
 const {PORT} = process.env
 
-router.get('/graphiql', graphiqlMiddleware(async (ctx) => {
-  return {
-    url: '/graphql'
-  }
-}))
-
-app.use(koaBody())
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 
 const schema = makeExecutableSchema({
   typeDefs: fs.readFileSync('./src/schema.graphql').toString(),
   resolvers
 })
 
-router.post('/graphql', graphqlKoa({ schema: schema }))
-router.get('/graphql', graphqlKoa({ schema: schema }))
+router.post('/graphql', koaBody(), graphqlKoa({ schema: schema }));
+router.get('/graphql', graphqlKoa({ schema: schema }));
 
 app.use(router.routes())
 app.use(router.allowedMethods())
